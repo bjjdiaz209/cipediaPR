742742438f5bde7ad9b4617222daefc05d200a1b
Juan209 <quintanajuancito34@gmail.com>
2024-07-08 23:10:19 -0400

    pe

---
+++ README.md
@@ -0,0 +1,6 @@
+# cinepediab
+
+# Dev
+
+1. copiar el .env.template y renombrarlo a .env
+2. cambiar las variables de entorno
---
+++ android/app/src/main/kotlin/com/example/cinepediab/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cinepediab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
