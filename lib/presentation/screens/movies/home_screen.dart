3c0d9134e854aa1fe3d4321875a35e4d93126694
Juan209 <quintanajuancito34@gmail.com>
2024-07-09 23:59:45 -0400

    se implemento mapper para tener control de la informacion de la API

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:cinepediab/config/constants/environment.dart';
+//import 'package:cinepediab/config/constants/environment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
