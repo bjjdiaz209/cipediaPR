722300bc71feb931ea469cce6c47e32c77711d96
Juan209 <quintanajuancito34@gmail.com>
2024-07-11 18:37:20 -0400

    se creo Appbar custom,y paginacion de peliculas card Swiper

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,5 @@
-import 'package:cinepediab/config/constants/environment.dart';
 import 'package:cinepediab/presentation/providers/movies/movies_providers.dart';
+import 'package:cinepediab/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -33,14 +33,11 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    return Column(
+      children: [
+        const CustomAppbar(),
+        MoviesSlideshow(movies: nowPlayingMovies)
+      ],
     );
   }
 }
