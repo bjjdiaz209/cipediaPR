838c34252a9438ae9e79bbb8e1b208fff2434a6c
Juan209 <quintanajuancito34@gmail.com>
2024-08-09 00:02:07 -0400

    search provider,state search,bulidresults,refact repeat ,loadind indicator

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,6 +1,7 @@
 import 'package:cinepediab/domain/entities/movie.dart';
 import 'package:cinepediab/presentation/delegates/search_movie_delegate.dart';
 import 'package:cinepediab/presentation/providers/movies/movie_repository_provider.dart';
+import 'package:cinepediab/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -26,12 +27,17 @@ class CustomAppbar extends ConsumerWidget {
             const Spacer(),
             IconButton(
                 onPressed: () {
-                  final movieRepository = ref.read(movieRepositoryProvider);
+                  final searchedMovies = ref.read(searchedMoviesProvider);
+                  final searchQuery = ref.read(searchQueryProvider);
 
                   showSearch<Movie?>(
+                          query: searchQuery,
                           context: context,
                           delegate: SearchMovieDelegate(
-                              searchMovies: movieRepository.searchMovies))
+                              initialMovies: searchedMovies,
+                              searchMovies: ref
+                                  .read(searchedMoviesProvider.notifier)
+                                  .searchMoviesByQuery))
                       .then((movie) {
                     if (movie == null) return;
 
