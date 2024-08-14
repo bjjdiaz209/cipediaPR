e0e39dafce5019da80f355382b398e310b6bf7d5
Juan209 <quintanajuancito34@gmail.com>
2024-08-13 22:47:35 -0400

    go router ofiacil implementacion

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,19 +1,51 @@
 import 'package:cinepediab/presentation/screens/screens.dart';
+import 'package:cinepediab/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
         GoRoute(
-            path: 'movie/:id',
-            name: MovieScreen.name,
+            path: '/',
             builder: (context, state) {
-              final movieId = state.pathParameters['id'] ?? 'no-id';
+              return const HomeView();
+            },
+            routes: [
+              GoRoute(
+                path: 'movie/:id',
+                name: MovieScreen.name,
+                builder: (context, state) {
+                  final movieId = state.pathParameters['id'] ?? 'no-id';
 
-              return MovieScreen(movieId: movieId);
-            }),
-      ]),
+                  return MovieScreen(movieId: movieId);
+                },
+              ),
+            ]),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        ),
+      ])
+
+//  GoRoute(
+  //    path: '/',
+  //  name: HomeScreen.name,
+  //builder: (context, state) => const HomeScreen(childView: HomeView()),
+  //routes: [
+  //GoRoute(
+  //  path: 'movie/:id',
+  //name: MovieScreen.name,
+  //builder: (context, state) {
+  //final movieId = state.pathParameters['id'] ?? 'no-id';
+
+  //return MovieScreen(movieId: movieId);
+  //    }
+
+  // ),
+  //]),
 ]);
---
+++ lib/presentation/views/home_views/home_view.dart
@@ -0,0 +1,83 @@
+import 'package:cinepediab/presentation/providers/providers.dart';
+import 'package:cinepediab/presentation/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeView extends ConsumerStatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  HomeViewState createState() => HomeViewState();
+}
+
+class HomeViewState extends ConsumerState<HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    ref.read(popularMoviesProvider.notifier).loadNextPage();
+    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
+    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
+  }
+
+  //rederizado del la lista
+  @override
+  Widget build(BuildContext context) {
+    final initialLoading = ref.watch(initialLoadingProvider);
+    if (initialLoading) return const FullScreenLoader();
+
+    final slideShowMovies = ref.watch(moviesSlideshowProvider);
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final popularMovies = ref.watch(popularMoviesProvider);
+    final topRatedMovies = ref.watch(topRatedMoviesProvider);
+    final upcomingMovies = ref.watch(upcomingMoviesProvider);
+
+    return CustomScrollView(
+      slivers: [
+        const SliverAppBar(
+          floating: true,
+          //elevation: 1,
+          flexibleSpace: FlexibleSpaceBar(
+            centerTitle: true,
+            title: CustomAppbar(),
+          ),
+        ),
+        SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+          return Column(
+            children: [
+              //const CustomAppbar(),
+              MoviesSlideshow(movies: slideShowMovies),
+              MovieHorizontalListview(
+                  movies: nowPlayingMovies,
+                  title: 'En Cines',
+                  subTittle: 'Lunes 20',
+                  loadNextPage: () => ref
+                      .read(nowPlayingMoviesProvider.notifier)
+                      .loadNextPage()),
+              MovieHorizontalListview(
+                  movies: upcomingMovies,
+                  title: 'proximamente',
+                  subTittle: 'este mes',
+                  loadNextPage: () =>
+                      ref.read(upcomingMoviesProvider.notifier).loadNextPage()),
+              MovieHorizontalListview(
+                  movies: popularMovies,
+                  title: 'Populares',
+                  //subTittle: '',
+                  loadNextPage: () =>
+                      ref.read(popularMoviesProvider.notifier).loadNextPage()),
+              MovieHorizontalListview(
+                  movies: topRatedMovies,
+                  title: 'Mejor Calificados',
+                  subTittle: 'de todos los tiempos',
+                  loadNextPage: () =>
+                      ref.read(topRatedMoviesProvider.notifier).loadNextPage()),
+              const SizedBox(height: 30),
+            ],
+          );
+        }, childCount: 1)),
+      ],
+    );
+  }
+}
