3d557b47fd497a0b6a3e015c8cf4f866c98fc8f5
Juan209 <quintanajuancito34@gmail.com>
2024-08-20 22:53:51 -0400

    botton fav,isarDatbaseconexion,repository Datasorce

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 
 1. copiar el .env.template y renombrarlo a .env
 2. cambiar las variables de entorno
+3. cambios en la entidad hay que ejecutar el comando
+``` flutter pub run build_runner build```
---
+++ lib/infrastucture/datasources/isar_datasource.dart
@@ -0,0 +1,42 @@
+import 'package:cinepediab/domain/datasources/local_storage_datasorce.dart';
+import 'package:cinepediab/domain/entities/movie.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+class IsarDatasource extends LocalStorageDatasorce {
+  late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openDB();
+  }
+  Future<Isar> openDB() async {
+    final dir = await getApplicationDocumentsDirectory();
+
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open(
+        [MovieSchema],
+        inspector: true,
+        directory: dir.path,
+      );
+    }
+    return Future.value(Isar.getInstance());
+  }
+
+  @override
+  Future<bool> isMovieFavorite(int movieId) {
+    // TODO: implement isMovieFavorite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
+    // TODO: implement loadMovies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) {
+    // TODO: implement toggleFavorite
+    throw UnimplementedError();
+  }
+}
